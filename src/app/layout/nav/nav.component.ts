40e427262405bb87a39e2d52d9e12d5029cf4cc8
Juan Pablo Ortiz <ortizjup@outlook.com>
2019-07-01 22:29:01 -0300

    fix in swal package wrong imports in angular.json

--- src/app/layout/nav/nav.component.ts
+++ src/app/layout/nav/nav.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-nav',
@@ -9,14 +11,50 @@ export class NavComponent implements OnInit {
 
   model: any = {};
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
 
   }
 
   login(){
-    console.log(this.model);
+    this.authService.login(this.model).subscribe(next => {
+      Swal.fire({
+        title: 'Login Succed!',
+        text: 'Welcome!',
+        type: 'success',
+        timer: 4000
+      });
+    }, error => {
+      Swal.fire({
+        title: 'Login Failed!',
+        text: error.message,
+        type: 'error',
+        showConfirmButton: false
+      });
+    });
+  }
+
+  loggedIn(){
+    const token = localStorage.getItem('token');
+    return !!token;
+  }
+
+  logOut(){
+    if(this.loggedIn()){
+      localStorage.removeItem('token');
+      Swal.fire({
+        title: 'Logged Out',
+        text: 'You have been logged out of Dating App',
+        type: 'success',
+        showConfirmButton: false,
+        timer: 3000
+      });
+    }
+  }
+
+  editProfile(){
+    console.log("Edit Profile");
   }
 
 }
